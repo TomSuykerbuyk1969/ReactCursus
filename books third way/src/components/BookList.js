acb0f4dfc7f6dce6ec897248aba5fb05625e7551
TomSuykerbuyk1969 <tom.suykerbuyk@ziggo.nl>
2023-04-14 11:43:53 +0200

    fix useeffect with usecallback

--- books third way/src/components/BookList.js
+++ books third way/src/components/BookList.js
@@ -4,7 +4,7 @@ import BookShow from "./BookShow";
 function BookList() {
     const { books } = useBooksContext();
     const rederedBooks = books.map((book) => {
-        return <BookShow Book={book}/>
+        return <BookShow key={book.id} Book={book}/>
     });
 
     return <div className="book-list">
